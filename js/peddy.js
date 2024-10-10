e4ed0a5053fdf8edd0a4b2831a23d88380a93812
ShoaibAlam107 <shoaib.cap107@gmail.com>
2024-10-10 17:57:13 +0600

    main section remake and event handlar into view more button

--- js/peddy.js
+++ js/peddy.js
@@ -1,4 +1,9 @@
-console.log('hey hi')
+
+const scrollToMain = () => {
+  document.getElementById("main-section").scrollIntoView({behavior:'smooth'})
+}
+
+
 
 const allCategories = ()=>{
     fetch('https://openapi.programming-hero.com/api/peddy/categories')
@@ -7,18 +12,41 @@ const allCategories = ()=>{
     .catch(err => console.log(err))
 }
 
+// "id": 1,
+//       "category": "Cat",
+//       "category_icon": "https://i.ibb.co.com/N7dM2K1/cat.png"
+
+const categoryCard =(category) =>{
+//  alert(id)
+ fetch(`https://openapi.programming-hero.com/api/peddy/category/${category}`)
+ .then(res => res.json())
+ .then(data => displayCategories(data.category))
+ .catch(err => console.log(err))
+}
+
 
 
 const displayCategories =(categories) => {
-    const categoryContainer = document.getElementById('categories')
-    categories.forEach(item => {
-        console.log(item)
-        const button = document.createElement('button');
-        button.classList = 'btn';
-        button.innerText = item.category;
-        categoryContainer.append(button)
-        
-    });
+  const categoryContainer = document.getElementById('categories')
+  categories.forEach(item => {
+      console.log(item)
+      const buttonContainer = document.createElement('div');
+      buttonContainer.innerHTML = 
+      `
+      <button onclick ="categoryCard('${item.category}')" class="btn">
+    <div class="lg:flex lg:gap-3 flex gap-1 ">
+    <img class="h-5" src="${item.category_icon}" alt="">
+    ${item.category}
+    </div>
+    </button>
+    `
+    categoryContainer.append(buttonContainer)
+  });
+      
+
+      
+      
+      
 
 }
 
@@ -52,7 +80,7 @@ const displayCards =(pets) => {
    pets.forEach(pet => {
     console.log(pet)
     const card =document.createElement('div')
-    card.classList = 'card card-compact w-64 '
+    card.classList = 'card card-compact md:w-48 lg:w-60 w-32 '
     card.innerHTML=
     `
     <figure class=" h-[160px] ">
@@ -64,23 +92,23 @@ const displayCards =(pets) => {
   <div class="px-0 py-2">
   <h2 class="font-bold text-xl">${pet.pet_name}</h2>
   <div class="flex gap-2">
-     <img class="w-3 h-4" src="images/magnification.png" alt="">
-  <p class="font-normal text-sm  "><span class="font-bold"> Breed :</span> ${pet.breed}</p> 
+     <img class="lg:w-3 lg:h-3 w-2 h-3 my-1" src="images/magnification.png" alt="">
+  <p class="font-normal lg:text-sm  text-xs "><span class="font-bold"> Breed :</span> ${pet.breed}</p> 
   </div>
 
   <div class="flex gap-2">
-    <img class="w-3 h-4" src="images/calendar.png" alt="">
-  <p class="font-normal text-sm "><span class="font-bold"> Birth :</span> ${pet.date_of_birth}</p>
+    <img class="lg:w-3 lg:h-3 w-2 h-3 my-1" src="images/calendar.png" alt="">
+  <p class="font-normal lg:text-sm  text-xs"><span class="font-bold"> Birth :</span> ${pet.date_of_birth}</p>
   </div>
 
   <div class="flex gap-2">
-  <img class="w-3 h-4" src="images/gender.png" alt="">
-   <p class="font-normal text-sm "><span class="font-bold"> Gender :</span> ${pet.gender}</p>
+  <img class="lg:w-3 lg:h-3 w-2 h-3 my-1" src="images/gender.png" alt="">
+   <p class="font-normal lg:text-sm  text-xs"><span class="font-bold"> Gender :</span> ${pet.gender}</p>
     </div>
 
    <div class="flex gap-2">
-   <img class="w-3 h-4" src="images/dollar-sign.png" alt="">
-    <p class="font-normal text-sm "><span class="font-bold"> Price :</span> ${pet.price} $</p>
+   <img class="lg:w-3 lg:h-3 w-2 h-3 my-1" src="images/dollar-sign.png" alt="">
+    <p class="font-normal lg:text-sm  text-xs "><span class="font-bold"> Price :</span> ${pet.price} $</p>
     </div>
     
   </div>
